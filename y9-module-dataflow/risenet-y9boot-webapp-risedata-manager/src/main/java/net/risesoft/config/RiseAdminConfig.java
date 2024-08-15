20e1ff16ba7abf3e6391f9b16355f001a635271c
Tobin <daobangshi@gmail.com>
2024-08-15 11:37:29 +0800

    refactor: 移除重复定义的 RequestContextFilter

--- y9-module-dataflow/risenet-y9boot-webapp-risedata-manager/src/main/java/net/risesoft/config/RiseAdminConfig.java
+++ y9-module-dataflow/risenet-y9boot-webapp-risedata-manager/src/main/java/net/risesoft/config/RiseAdminConfig.java
@@ -4,14 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.boot.web.servlet.filter.OrderedRequestContextFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.web.filter.RequestContextFilter;
 
 import net.risesoft.converter.EncryptConverter;
 import net.risesoft.y9.Y9Context;
@@ -19,14 +17,6 @@ import net.risesoft.y9.Y9Context;
 @Configuration
 @EnableAsync
 public class RiseAdminConfig {
-
-	// starter-log工程用到了RequestContextHolder
-	// https://github.com/spring-projects/spring-boot/issues/2637
-	// https://github.com/spring-projects/spring-boot/issues/4331
-	@Bean
-	public static RequestContextFilter requestContextFilter() {
-		return new OrderedRequestContextFilter();
-	}
 
 	@Bean
 	public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
