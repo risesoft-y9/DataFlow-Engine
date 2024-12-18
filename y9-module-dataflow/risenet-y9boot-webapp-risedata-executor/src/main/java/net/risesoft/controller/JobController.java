b02557ea91bb4bc1e79da5d52f9579e9e90d9e20
lb <1833953905@qq.com>
2024-12-18 10:06:52 +0800

    将执行中的任务进行保存登记

--- y9-module-dataflow/risenet-y9boot-webapp-risedata-executor/src/main/java/net/risesoft/controller/JobController.java
+++ y9-module-dataflow/risenet-y9boot-webapp-risedata-executor/src/main/java/net/risesoft/controller/JobController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import net.risedata.rpc.consumer.annotation.Listener;
 import net.risedata.rpc.consumer.annotation.Listeners;
+import net.risedata.rpc.consumer.listener.ListenerDispatch;
+import net.risedata.rpc.consumer.result.SyncResult;
 import net.risedata.rpc.utils.IdUtils;
 import net.risesoft.api.ConfigApi;
 import net.risesoft.model.Config;
@@ -36,12 +38,12 @@ import risesoft.data.transfer.core.util.Configuration;
 @Listeners
 @RestController
 public class JobController {
-	
+
 	/**
 	 * 执行任务的缓存配置
 	 */
 	private static Map<String, JobContext> JOB_CONTEXT = new ConcurrentHashMap<String, JobContext>();
-	
+
 	@Resource
 	private ConfigApi configApi;
 
@@ -113,6 +115,7 @@ public class JobController {
 	 * @return
 	 */
 	@GetMapping("/getStatus")
+	@Listener("getStatus")
 	public Map<String, Object> getStatus(String jobId) {
 		Map<String, Object> result = new HashMap<String, Object>();
 		JobContext jobContext = JOB_CONTEXT.get(jobId);
@@ -129,38 +132,88 @@ public class JobController {
 	}
 
 	/**
-	 * 停止任务
+	 * 停止
 	 * 
 	 * @param jobId
-	 * @param message
 	 * @return
 	 */
-	@GetMapping("/killJob")
-	public Map<String, Object> killJob(String jobId, String message) {
+	@Listener("stop")
+	@GetMapping("/stop")
+	public Map<String, Object> stop(String jobId, String msg) {
 		Map<String, Object> result = new HashMap<String, Object>();
 		JobContext jobContext = JOB_CONTEXT.get(jobId);
 		if (jobContext == null) {
 			result.put("success", false);
 			result.put("msg", "任务不存在，或已经失效!");
-		} // 停止失败需要提供停止方法
-
-		jobContext.getCommunication().setState(State.FAILED);
-		jobContext.getCommunication().setThrowable(new RuntimeException(message));
-		Engine.onJobFlush(jobContext);
-		result.put("success", true);
-		result.put("msg", "已停止!");
+		}
+		jobContext.stop(new RuntimeException(msg));
+		Communication communication = jobContext.getCommunication();
+		result.put("success", communication.getState() == State.SUCCEEDED);
+		result.put("status", communication.getState());
+		result.put("msg", communication.getState() == State.FAILED ? communication.getThrowableMessage()
+				: CommunicationTool.getStatistics(communication));
 		return result;
 	}
 
+	/**
+	 * 暂停
+	 * @param jobId
+	 * @param msg
+	 * @return
+	 */
+	@Listener("await")
+	@GetMapping("/await")
+	public Map<String, Object> await(String jobId, String msg) {
+		Map<String, Object> result = new HashMap<String, Object>();
+		JobContext jobContext = JOB_CONTEXT.get(jobId);
+		if (jobContext == null) {
+			result.put("success", false);
+			result.put("msg", "任务不存在，或已经失效!");
+		}
+		jobContext.await(msg);
+		Communication communication = jobContext.getCommunication();
+		result.put("success", communication.getState() == State.SUCCEEDED);
+		result.put("status", communication.getState());
+		result.put("msg", communication.getState() == State.FAILED ? communication.getThrowableMessage()
+				: CommunicationTool.getStatistics(communication));
+		return result;
+	}
+	/**
+	 * 启动
+	 * @param jobId
+	 * @param msg
+	 * @return
+	 */
+	@GetMapping("/runing")
+	@Listener("runing")
+	public Map<String, Object> runing(String jobId) {
+		Map<String, Object> result = new HashMap<String, Object>();
+		JobContext jobContext = JOB_CONTEXT.get(jobId);
+		if (jobContext == null) {
+			result.put("success", false);
+			result.put("msg", "任务不存在，或已经失效!");
+		}
+		jobContext.running();
+		Communication communication = jobContext.getCommunication();
+		result.put("success", communication.getState() == State.SUCCEEDED);
+		result.put("status", communication.getState());
+		result.put("msg", communication.getState() == State.FAILED ? communication.getThrowableMessage()
+				: CommunicationTool.getStatistics(communication));
+		return result;
+	}
+	
+
+
 	@Listener("awaitExecutorJobs")
-	public String awaitExecutorJobs(String args) {
+	public String awaitExecutorJobs(String args,String jobLogId) {
+
 		String[] ids = args.split(",");
 		StringBuilder stringBuilder = new StringBuilder();
 		for (int i = 0; i < ids.length; i++) {
-			System.out.println("获取任务id：" +ids[i]);
+			System.out.println("获取任务id：" + ids[i]);
 			// 获取任务类型
 			Integer taskType = configApi.taskType(ids[i]);
-			if(taskType == 1) {// 单节点任务
+			if (taskType == 1) {// 单节点任务
 				Config config = configApi.getSingleConfig(ids[i]);
 				if (config == null || config.getContent() == null) {
 					throw new RuntimeException("任务" + ids[i] + "不存在配置");
@@ -168,13 +221,14 @@ public class JobController {
 				// 执行任务
 				String msg = HandleSingleJobService.handle(config);
 				stringBuilder.append("任务:" + config.getName() + "\n" + msg);
-			}else {// 同步任务
+			} else {// 同步任务
 				Config config = configApi.getConfig(ids[i]);
 				if (config == null || config.getContent() == null) {
 					throw new RuntimeException("任务" + ids[i] + "不存在配置,这通常的程序问题请联系开发人员!");
 				}
-				System.out.println("执行任务"+config.getContent());
+				System.out.println("执行任务" + config.getContent());
 				ResultJobListener resultJobListener = Engine.start(ids[i], Configuration.from(config.getContent()));
+                JOB_CONTEXT.put(jobLogId, resultJobListener.getJobContext());
 				if (resultJobListener.isSuccess()) {
 					stringBuilder.append("任务:" + config.getName() + "执行成功\n" + resultJobListener.getMessage());
 				} else {
@@ -182,6 +236,8 @@ public class JobController {
 				}
 			}
 		}
+//		new syncres
+
 		System.out.println(stringBuilder);
 		return stringBuilder.toString();
 	}
