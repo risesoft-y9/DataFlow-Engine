49462f533edb85213e25be0931b1db1f5f4fddfd
lb <1833953905@qq.com>
2025-08-20 11:13:20 +0800

    缓存组件修改为泛型

--- y9-module-dataflow/risenet-y9boot-data-transfer-core/src/main/java/risesoft/data/transfer/core/handle/cache/CacheHandle.java
+++ y9-module-dataflow/risenet-y9boot-data-transfer-core/src/main/java/risesoft/data/transfer/core/handle/cache/CacheHandle.java
@@ -7,8 +7,7 @@ import risesoft.data.transfer.core.handle.Handle;
 import risesoft.data.transfer.core.plug.RootPlug;
 
 /**
- * 缓存插件
- * 实现这个插件的对象需要实现缓存方法，缓存组件用于任务的部分参数持久化以及提供给更多方法使用
+ * 缓存插件 实现这个插件的对象需要实现缓存方法，缓存组件用于任务的部分参数持久化以及提供给更多方法使用
  * 
  * @typeName CacheHandle
  * @date 2025年8月18日
@@ -22,41 +21,44 @@ public interface CacheHandle extends RootPlug, Handle {
 
 	/**
 	 * 根据key获取一个参数
+	 * 
 	 * @param key key
 	 * @return
 	 */
-	String get(String key);
-	
+	<T> T get(String key, Class<T> returnClass);
+
 	/**
 	 * 添加一个参数
-	 * @param key key
+	 * 
+	 * @param key   key
 	 * @param value value
 	 */
-	void put(String key,String value);
- 
+	void put(String key, Object value);
+
 	/**
 	 * 追加一个值，使用这个方法时为数组队列模式
-	 * @param key key
+	 * 
+	 * @param key   key
 	 * @param value value
 	 * @return 返回数组大小
 	 */
-	int push(String key,String value);
-	
+	int push(String key, Object value);
+
 	/**
-	 * 将key中的值出栈 
-	 * @param key key
+	 * 将key中的值出栈
+	 * 
+	 * @param key  key
 	 * @param size 拉取的数量
 	 * @return
 	 */
-	List<String> poll(String key,int size);
+	<T> List<T> poll(String key, int size, Class<T> returnClass);
 
-	
 	/**
 	 * 删除一个key
+	 * 
 	 * @param key
 	 * @return 是否存在
 	 */
 	boolean remove(String key);
-	
-	
+
 }
